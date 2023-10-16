06ccd7ed4d161a1d4721b3bea1152b132fb3acce
m.hanan <mohammadhannan00@gmail.com>
2023-10-16 12:22:28 +0500

    Refactor Day 2 Code

--- src/main/Constants/app-constant.ts
+++ src/main/Constants/app-constant.ts
@@ -3,4 +3,8 @@ export const AppConstants = {
   Morning: 'Morning',
   Afternoon: 'Afternoon',
   Evening: 'Evening',
+  Today: 'Today',
+  Yesterday: 'Yesterday',
+  Tomorrow: 'Tomorrow',
+  Alphabetical: 'Alphabetical',
 };
